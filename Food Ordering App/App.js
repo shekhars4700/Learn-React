c433bdad59ada38ea749aef2864074ff23b212d8
shekhars4700 <shekhars4700@gmail.com>
2024-07-30 21:18:53 -0500

    Using map with components example

--- Food Ordering App/App.js
+++ Food Ordering App/App.js
@@ -1,21 +1,70 @@
 import React from "react";
 import  ReactDom from "react-dom"
+import data from "./UberEatsData.json"
 
-// React Element
-const Title = ( <h1>
-    Hello From title JSX
-    </h1>)
+const HeaderComponent = () => {
+    return (
+    <div className="header">
+        <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq6FvMgTV6lXSizDbBkn9L3cLoxjdvK8O9EOVIxg7O129OafrA-qkm-8bzxwge-vUcXvg&usqp=CAU">
+        </img>
+        <div className="nav-bar">
+        <ul >
+            <li>Home</li>
+            <li>Profile</li>
+            <li>Contact</li>
+            <li>Cart</li>
+        </ul>
+        </div>
+    </div>)
+}
 
-const a = 1;
 
-// React Components 
-// Everything is a component in REACT
-const HeadingComponent = () => (
-    <div>
-        {/* <Title/> */}
-        {Title}
-        <h1>Hello from Functional Component</h1>
+
+
+const RestaurantCard = (props) => {
+    const {resData} = props;
+    
+    const
+    
+    return(
+        <div className="res-card">
+            <img className="res-card-img" src="https://media.cnn.com/api/v1/images/stellar/prod/140430115517-06-comfort-foods.jpg?q=w_1110,c_fill" >
+            </img>
+            <h3>Meghana Foods</h3>
+            <h4>Italian</h4>
+            <h4>3.2/5</h4>
+        </div>
+    )
+}
+
+
+
+
+const BodyComponent = () =>{
+    return(
+        <div className="body">
+            <div className="Search"> Search </div>
+                <div className="res-container">
+                {
+                    data.map(res => <RestaurantCard key={res.uuid} resData = {res}/> )
+                }    
+                
+            </div>
+        </div>
+    )
+}
+
+
+
+
+const MainAppComponent = () => {
+    return (
+        <div>
+    <HeaderComponent/>
+    <BodyComponent/>
     </div>
-)
+    )
+}
+
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(HeadingComponent() )
+root.render(<MainAppComponent/>)
