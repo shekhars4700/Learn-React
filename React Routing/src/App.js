05e028ece5a54a0f42b8ba1757f427d567e3c78a
shekhars4700 <shekhars4700@gmail.com>
2024-08-03 12:12:16 -0500

    Added About us Route

--- React Routing/src/App.js
+++ React Routing/src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import  ReactDom from "react-dom"
 import Header from "./components/Header.js"
 import Body from "./components/Body.js"
+
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import About from "./components/About.js";
+
+
 const MainApp = () => {
     return (
         <div>
@@ -9,6 +14,18 @@ const MainApp = () => {
     <Body/>
     </div>
     )
-}
+};
+
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element: <MainApp/>
+
+    },{
+        path:"/About",
+        element: <About/>
+    }
+]);
+
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<MainApp/>)
+root.render(<RouterProvider router={appRouter}/>);
