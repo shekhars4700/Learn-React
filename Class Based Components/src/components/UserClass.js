a14c75966addc7fc87d1d383004a0aa6363e4b32
shekhars4700 <shekhars4700@gmail.com>
2024-08-11 18:15:35 -0500

    Class Components and life cycle

--- Class Based Components/src/components/UserClass.js
+++ Class Based Components/src/components/UserClass.js
@@ -1,21 +1,35 @@
-import React from "react";
+import {Component} from "react";
 
-class UserClass extends React.Component {
-
+class UserClass extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            count :0
+        }
     }
 
     render(){
         return (
             <div className="User-Card">
-                <h2>Name: {this.props.name}</h2>
-                <h3>Location: {this.props.location}</h3>
+                <img src = {this.state.profileImageURL}></img>
+                <h2>Name: {this.state.name}</h2>
+                <h3>Location: {this.state.location}</h3>
             </div>
         )
-
-
     }
+    
+    async componentDidMount(){
+        const githubUserDataURL = "https://api.github.com/users/shekhars4700"
+        const apiResult = await fetch(githubUserDataURL);
+        const gitUserData = await apiResult.json(); 
+        this.setState({
+            name :gitUserData.name,
+            location :gitUserData.location, 
+            profileImageURL:gitUserData.avatar_url
+        })
+    }
+
+
 }
 
 export default UserClass;
